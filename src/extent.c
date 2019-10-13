69715b2ff48366a366ebd94c7edb4a38f0de9548
Park Ju Hyung <qkrwngud825@gmail.com>
2019-10-14 06:19:00 +0900

    jemalloc: Remove THP support
    This is not explored on Android properly and no one should use it until
    proper scientific testing.
    
    Moreover, THP is broken with all recent Qualcomm devices due to
    speculative page faults.
    
    z3DD3r: Adopted for jemalloc 5.1.0
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- src/extent.c
+++ src/extent.c
@@ -1186,9 +1186,6 @@ extent_alloc_default_impl(tsdn_t *tsdn, arena_t *arena, void *new_addr,
 	void *ret = extent_alloc_core(tsdn, arena, new_addr, size, alignment, zero,
 	    commit, (dss_prec_t)atomic_load_u(&arena->dss_prec,
 	    ATOMIC_RELAXED));
-	if (have_madvise_huge && ret) {
-		pages_set_thp_state(ret, size);
-	}
 	return ret;
 }
 
