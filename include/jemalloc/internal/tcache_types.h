aosp-riscv/platform_external_jemalloc_new

Add android extensions.
Bug: 62621531

Test: Ran unit tests and benchmarks using libc.

Change-Id: Ie13ab8510c42f96b58496b0ab7e4f8c3a9cd2c6d

include/jemalloc/internal/tcache_types.h
@@ -19,7 +19,11 @@ typedef struct tcaches_s tcaches_t;
 /*
  * Absolute minimum number of cache slots for each small bin.
  */
+#if defined(ANDROID_TCACHE_NSLOTS_SMALL_MIN)
+#define TCACHE_NSLOTS_SMALL_MIN		ANDROID_TCACHE_NSLOTS_SMALL_MIN
+#else
 #define TCACHE_NSLOTS_SMALL_MIN		20
+#endif
 
 /*
  * Absolute maximum number of cache slots for each small bin in the thread
@@ -28,13 +32,25 @@ typedef struct tcaches_s tcaches_t;
  *
  * This constant must be an even number.
  */
+#if defined(ANDROID_TCACHE_NSLOTS_SMALL_MAX)
+#define TCACHE_NSLOTS_SMALL_MAX		ANDROID_TCACHE_NSLOTS_SMALL_MAX
+#else
 #define TCACHE_NSLOTS_SMALL_MAX		200
+#endif
 
 /* Number of cache slots for large size classes. */
+#if defined(ANDROID_TCACHE_NSLOTS_LARGE)
+#define TCACHE_NSLOTS_LARGE		ANDROID_TCACHE_NSLOTS_LARGE
+#else
 #define TCACHE_NSLOTS_LARGE		20
+#endif
 
 /* (1U << opt_lg_tcache_max) is used to compute tcache_maxclass. */
+#if defined(ANDROID_LG_TCACHE_MAXCLASS_DEFAULT)
+#define LG_TCACHE_MAXCLASS_DEFAULT	ANDROID_LG_TCACHE_MAXCLASS_DEFAULT
+#else
 #define LG_TCACHE_MAXCLASS_DEFAULT	15
+#endif
 
 /*
  * TCACHE_GC_SWEEP is the approximate number of allocation events between

z3DD3r/android_external_jemalloc_new

Bug: 110158834

include/jemalloc/internal/tcache_types.h
@@ -27,7 +27,11 @@ typedef struct tcaches_s tcaches_t;
 /* Used for explicit tcache only. Means flushed but not destroyed. */
 #define TCACHES_ELM_NEED_REINIT ((tcache_t *)(uintptr_t)1)
 
+#if defined(ANDROID_LG_TCACHE_MAXCLASS_DEFAULT)
+#define TCACHE_LG_MAXCLASS_LIMIT	ANDROID_LG_TCACHE_MAXCLASS_DEFAULT
+#else
 #define TCACHE_LG_MAXCLASS_LIMIT 23 /* tcache_maxclass = 8M */
+#endif
 #define TCACHE_MAXCLASS_LIMIT ((size_t)1 << TCACHE_LG_MAXCLASS_LIMIT)
 #define TCACHE_NBINS_MAX (SC_NBINS + SC_NGROUP *			\
     (TCACHE_LG_MAXCLASS_LIMIT - SC_LG_LARGE_MINCLASS) + 1)