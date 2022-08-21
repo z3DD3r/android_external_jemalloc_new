aosp-riscv/platform_external_jemalloc_new

Further updates to jemalloc code.
Add support for svelte.

Add je_iterate support.

Update some of the internals so that bad pointers in je_iterate do not
crash.

Test: Ran new bionic unit tests, ran libmemunreachable tests, booted system.
Change-Id: I04171cf88df16d8dc2c2ebb60327e58b915b9d83

src/tcache.c
@@ -9,7 +9,11 @@
 /******************************************************************************/
 /* Data. */
 
+#if !defined(__ANDROID__) || defined(ANDROID_ENABLE_TCACHE)
 bool	opt_tcache = true;
+#else
+bool	opt_tcache = false;
+#endif
 ssize_t	opt_lg_tcache_max = LG_TCACHE_MAXCLASS_DEFAULT;
 
 cache_bin_info_t	*tcache_bin_info;

z3DD3r/android_external_jemalloc_new

src/tcache.c
@@ -10,8 +10,11 @@
 /******************************************************************************/
 /* Data. */
 
+#if !defined(__BIONIC__) || defined(ANDROID_ENABLE_TCACHE)
 bool opt_tcache = true;
-
+#else
+bool opt_tcache = false;
+#endif
 /* tcache_maxclass is set to 32KB by default.  */
 size_t opt_tcache_max = ((size_t)1) << 15;
 